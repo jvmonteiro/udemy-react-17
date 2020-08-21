3d6de7aee2517399f9873509d21fb52288b72b80
João Vítor Monteiro <jvmonteirorj@gmail.com>
2020-08-21 12:44:18 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./components/App";
+import reducers from "./reducers";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  rootElement
+);
